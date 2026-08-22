a31ce4e6a0f31946997ee4756bbed742d6a47548
max1234-cyber <maxime.boucher+github@gmail.com>
2026-08-22 14:16:16 -0400

    Add constants for gaposa_linkit component

---
+++ custom_components/gaposa_linkit/const.py
@@ -0,0 +1,11 @@
+DOMAIN = "gaposa_linkit"
+DEFAULT_PORT = 4999  # Standard iTach TCP port for serial
+CONF_HOST = "host"
+CONF_PORT = "port"
+
+# Command Bytes
+CMD_UP = 0xdd
+CMD_DOWN = 0xee
+CMD_STOP = 0xcc
+CMD_PAIR = 0xaa
+CMD_UNPAIR = 0xab
